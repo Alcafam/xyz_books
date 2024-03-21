7fe3390f66a41709cefb5c04f76acb23a93e39dd
Alcafam <90877974+Alcafam@users.noreply.github.com>
2024-03-21 22:44:04 +0800

    first commit

---
+++ app/models/book.rb
@@ -0,0 +1,4 @@
+class Book < ApplicationRecord
+  belongs_to :publishers
+  has_many :authors, through :author_books
+end
---
+++ db/migrate/20240321143856_create_books.rb
@@ -0,0 +1,15 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :isbn
+      t.decimal :price
+      t.integer :year_published
+      t.text :image
+      t.string :edition
+      t.references :publisher, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
